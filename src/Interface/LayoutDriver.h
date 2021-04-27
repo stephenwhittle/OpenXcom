daabf6bb2dbfc2acec3ede93c375668318f5fcb5
Stephen Whittle <swhittle@crypticwarrior.net>
2021-04-27 15:35:37 +1000

    Documentation comments for LayoutDriver, LayoutGroup

--- src/Interface/LayoutDriver.h
+++ src/Interface/LayoutDriver.h
@@ -25,12 +25,19 @@ namespace OpenXcom
 		LayoutAlignment alignment = LayoutAlignment::Default;
 
 		LayoutParam(Surface* Widget);
+		/// @brief indicates the widget's size should not be altered at all, only its position
 		LayoutParam& KeepSize();
+		/// @brief indicates the widget should be proportionally resized in the remaining space
+		/// along the primary axis after absolute size widgets have space allocated
 		LayoutParam& Proportional(float XProportion, float YProportion);
-
+		/// @brief indicates the widget should have its size along the primary axis fixed to
+		/// the specified value
 		LayoutParam& Absolute(float XSize, float YSize);
+		/// @brief makes the off-axis size of the widget equal to the maximum available space
 		LayoutParam& OtherAxisStretch();
+		/// @brief positions the widget along the center of the off-axis
 		LayoutParam& OtherAxisCenter();
+
 		Surface* getWidget() const;
 	};
 
@@ -39,7 +46,12 @@ namespace OpenXcom
 		Horizontal,
 		Vertical
 	};
-
+	/// @brief Class to arrange a set of widgets either horizontally or vertically
+	/// Given a parent surface, allocates space for all absolute sized widgets in the given direction, then proportionally
+	/// allocates the remaining space between the proportional widgets based on the relative values given for their size in that axis.
+	/// By default, places the widgets at the start of the other axis and does not alter their size. If specified will set the other
+	/// axis' dimension to the maximum amount of free space, filling (OtherAxisStretch) or will centre the widget along that other axis
+	/// (OtherAxisCenter).
 	class LayoutDriver
 	{
 	private:
@@ -59,6 +71,9 @@ namespace OpenXcom
 		/// Helper method to make ApplyLayout a little more terse
 		bool Horizontal();
 	public:
+		/// @brief Constructs a LayoutDriver that will use the specified primary axis and the
+		///bounding box of the specified parent surface to lay children out based on
+		/// LayoutParams rules
 		template<typename... Params>
 		LayoutDriver(LayoutDirection Direction, Surface* Parent, Params... Children)
 			:_rootElement(Parent),
@@ -67,9 +82,13 @@ namespace OpenXcom
 		{
 
 		}
-
+		/// @brief allocates the specified number of pixels around all edges of the parent widget
+		/// before performing layout of children
 		LayoutDriver& Padding(int AllValue);
+		/// @brief allocates the specified number of pixels around all edges of the parent widget
+		/// before performing layout of children
 		LayoutDriver& Padding(int VPadding, int HPadding);
+		/// @brief resizes and positions the widgets that were wrapped in the LayoutParams passed to the constructor
 		void ApplyLayout();
 	};
 
--- src/Interface/LayoutGroup.h
+++ src/Interface/LayoutGroup.h
@@ -4,7 +4,10 @@
 
 namespace OpenXcom
 {
-
+	/// @brief Surface that will manage a set of sibling widgets with a layout driver
+	/// This allows for the nesting of layout drivers
+	/// Setting the position or size of this surface will cause the managed sibling widgets
+	/// to have their position and size recalculated
 	class LayoutGroup : public Surface
 	{
 	private:
@@ -17,12 +20,14 @@ namespace OpenXcom
 
 
 	public:
+		/// @brief Static factory method for constructing a LayoutGroup with a layout driver controlling the vertical axis as primary
 		template<typename... Params>
 		static LayoutGroup* Vertical(int width, int height, Params... Children)
 		{
 			return new LayoutGroup(width, height, OpenXcom::LayoutDirection::Vertical, std::forward<Params>(Children)...);
 		}
 
+		/// @brief Static factory method for constructing a LayoutGroup with a layout driver controlling the horizontal axis as primary
 		template<typename... Params>
 		static LayoutGroup* Horizontal(int width, int height, Params... Children)
 		{
